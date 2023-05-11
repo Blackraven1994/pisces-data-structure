ce32f630d19d7b7c2a99b3ab1d241f0e2a7a8f48
lizexing <1487030307@qq.com>
2023-05-11 09:56:15 +0800

    feat: 新增入口文件 `index.ts`

---
+++ index.ts
@@ -0,0 +1,65 @@
+/**
+ * 类型/接口
+ */
+export type {
+    IList,
+
+    IStack,
+
+    IQueue,
+    IDEQueue,
+    IPriorityNode,
+    IPriorityQueue,
+
+    targetNode,
+    INode,
+    IDoublyLinkedNode,
+    IDoublePointNode,
+    ILinkedList,
+    ISinglyCircularLinkedList,
+    IDoublyLinkedList,
+
+    ERedBlackTreeNodeColor,
+    ITreeNode,
+    IBinarySearchTreeNode,
+    IAVLTreeNode,
+    IRedBlackTreeNode,
+    ITree,
+    IBinarySearchTree,
+    IAVLTree,
+    IRedBlackTree,
+
+    THeapType,
+    IHeap,
+} from './src';
+
+/**
+ * 实现的数据结构
+ */
+export {
+    Stack,
+
+    SinglyQueue,
+    DEQueue,
+    PriorityQueue,
+
+    Heap,
+
+    SinglyLinkedNode,
+    SinglyLinkedList,
+    DoublyLinkedNode,
+    DoublyLinkedList,
+    SinglyCircularLinkedList,
+
+    HashTable,
+
+    Vertex,
+    Graph,
+
+    BinarySearchTreeNode,
+    BinarySearchTree,
+    AVLTreeNode,
+    AVLTree,
+    RedBlackTreeNode,
+    RedBlackTree,
+} from './src';
